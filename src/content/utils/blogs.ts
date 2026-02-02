b05e9b1f14f80bed05e538b6392d2756eaf99b52
erkobridee <erko.bridee@gmail.com>
2026-02-02 17:06:15 +0100

    feat(pages): split the strapi loaded content by locale

--- src/content/utils/blogs.ts
+++ src/content/utils/blogs.ts
@@ -6,7 +6,20 @@ import { isCIEnv } from '~/utils/env';
 
 //----------------------------------------------------------------------------//
 
-type TCollectionEntry = CollectionEntry<'blogs'>;
+export type TCollectionEntry = CollectionEntry<'blogs'>;
 
 export const getEntries = async (): Promise<TCollectionEntry[]> =>
   isCIEnv ? [] : await getCollection('blogs');
+
+export const mapByLocale = (entries: TCollectionEntry[]) =>
+  entries.reduce((acc, entry) => {
+    const locale = entry.data.locale;
+
+    if (acc.has(locale)) {
+      acc.get(locale).push(entry);
+    } else {
+      acc.set(locale, [entry]);
+    }
+
+    return acc;
+  }, new Map<String, TCollectionEntry[]>());
--- src/content/utils/pages.ts
+++ src/content/utils/pages.ts
@@ -6,7 +6,20 @@ import { isCIEnv } from '~/utils/env';
 
 //----------------------------------------------------------------------------//
 
-type TCollectionEntry = CollectionEntry<'pages'>;
+export type TCollectionEntry = CollectionEntry<'pages'>;
 
 export const getEntries = async (): Promise<TCollectionEntry[]> =>
   isCIEnv ? [] : await getCollection('pages');
+
+export const mapByLocale = (entries: TCollectionEntry[]) =>
+  entries.reduce((acc, entry) => {
+    const locale = entry.data.locale;
+
+    if (acc.has(locale)) {
+      acc.get(locale).push(entry);
+    } else {
+      acc.set(locale, [entry]);
+    }
+
+    return acc;
+  }, new Map<String, TCollectionEntry[]>());
